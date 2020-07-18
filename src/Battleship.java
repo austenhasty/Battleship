b0da15f35e31d3f9f9ba71a821c6d5bbb078916f
austenhasty <hastyae@gmail.com>
2020-07-18 12:03:35 -0700

    draw blank sea map

--- src/Battleship.java
+++ src/Battleship.java
@@ -1,5 +1,26 @@
-public class Battleship {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Battleship {
+    private static String[][] sea = new String[10][10]
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("****Welcome to Battleships Game****");
+        System.out.println();
+        System.out.print(drawSea());
+
+
+    }
+
+    public static void drawSea() {
+        System.out.println("   0123456789");
+        for (int i = 0; i < 10; i++) {
+            System.out.print(i + " |");
+            for (int j = 0; j < 10; j++) {
+                sea[i][j] = " ";
+                System.out.print(sea[i][j]);
+            }
+            System.out.print("| " + i);
+        }
+        System.out.println("   0123456789")
     }
 }
